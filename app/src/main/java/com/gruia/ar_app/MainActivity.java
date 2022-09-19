3a263532f296b6a323369e12c17aaa534483b8d4
Gruia Pana <gruiapana99@gmail.com>
2022-09-19 15:45:48 +0300

    -Landing page design update

--- app/src/main/java/com/gruia/ar_app/MainActivity.java
+++ app/src/main/java/com/gruia/ar_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.gruia.ar_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -17,11 +18,15 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static Activity fa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        fa = this;
 
 
 
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     public void startButtonClicked(View v)
     {
         Intent intent = new Intent(this, MainPage.class);
+        MainActivity.fa.finish();
         startActivity(intent);
     }
 }
