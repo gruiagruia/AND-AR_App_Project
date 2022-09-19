2ab3474bd0d1c154a2c6bdb9e097e6770acb5bc8
Gruia Pana <gruiapana99@gmail.com>
2022-09-19 11:33:14 +0200

    -Designed the main view -Designed the dashboard

--- app/src/main/java/com/gruia/ar_app/MainActivity.java
+++ app/src/main/java/com/gruia/ar_app/MainActivity.java
@@ -2,13 +2,34 @@ package com.gruia.ar_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void startButtonClicked(View v)
+    {
+        Intent intent = new Intent(this, MainPage.class);
+        startActivity(intent);
     }
 }
