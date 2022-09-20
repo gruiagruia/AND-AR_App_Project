67366c976d67ac89dc1b7cfec8cefc5b3ec7ece1
Gruia Pana <gruiapana99@gmail.com>
2022-09-20 11:04:10 +0300

    - updated the design for profile activity - updated the design for progress activity

--- app/src/main/java/com/gruia/ar_app/CoursesActivity.java
+++ app/src/main/java/com/gruia/ar_app/CoursesActivity.java
@@ -3,12 +3,14 @@ package com.gruia.ar_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class CoursesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_courses);
     }
 }
